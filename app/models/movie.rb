a53ee1bbd34070d1b9db995c658034ddad2dd013
Eric <echien@uncc.edu>
2016-10-26 13:37:14 +0000

    Part 3 Complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,14 @@
 class Movie < ActiveRecord::Base
   
   
+  def self.movies(choice)
+    self.order(choice)
+  end
+  
   def self.all_ratings
     a = Array.new
-    self.select('rating').uniq.each {|x| a.push(x.rating)}
-    a.sort_by{|rating| rating.to_s}.uniq
+    self.select("rating").uniq.each{|x| a.push(x.rating)}
+    a.sort.uniq
   end
-
   
 end
