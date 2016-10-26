180d83358b80787283b02327e745100b0d8594b8
Eric <echien@uncc.edu>
2016-10-26 04:41:30 +0000

    Part 3 Complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,51 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    redirect = false
+    logger.debug(session.inspect)
+    if params[:sort_by]
+      @sort_by = params[:sort_by]
+      session[:sort_by] = params[:sort_by]
+    elsif session[:sort_by]
+      @sort_by = session[:sort_by]
+      redirect = true
+    else
+      @sort_by = nil
+    end
+    
+    if params[:commit] == "Refresh" and params[:ratings].nil?
+      @ratings = nil
+      session[:ratings] = nil
+    elsif params[:ratings]
+      @ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      redirect = true
+    else
+      @ratings = nil
+    end
+    
+    if redirect
+      flash.keep
+      redirect_to movies_path :sort_by => @sort_by, :ratings => @ratings
+    end
+    
+    if @ratings and @sort_by
+      @movies = Movie.where(:rating => @ratings.keys).order(@sort_by)
+    elsif @ratings
+      @movies = Movie.where(:rating => @ratings.keys)
+    elsif @sort_by
+      @movies = Movie.order(@sort_by)
+    else
+      @movies = Movie.all
+    end
+    
+    if !@set_ratings
+      @set_ratings = Hash.new
+    end
+    
   end
 
   def new
@@ -39,6 +83,12 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+  
+  def sort_by
+    @movies = Movie.order(:title).first
+    flash[:notice] = "#{@movie.title} was successfully sorted by name."
     redirect_to movies_path
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+  
+  
+  def self.all_ratings
+    a = Array.new
+    self.select('rating').uniq.each {|x| a.push(x.rating)}
+    a.sort_by{|rating| rating.to_s}.uniq
+  end
+
+  
 end
